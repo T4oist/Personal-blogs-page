157c485c7adc58691a4de4f520aff8697705a35f
T4oist <ziteng_wang611@163.com>
2026-06-29 01:49:04 +0800

    Add note post without description

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const blog = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().optional(),
     date: z.coerce.date(),
     updated: z.coerce.date().optional(),
     tags: z.array(z.string()).default([]),
